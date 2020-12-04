834a5c7fca9fab8bd4f6ddcadcc408b57312c7d8
Dlegostaev <smashzoomik@gmail.com>
2020-12-04 23:07:54 +0300

    Edited UITest according to current UI module layout

--- src/test/java/ru/open/ui/UITest.java
+++ src/test/java/ru/open/ui/UITest.java
@@ -1,4 +1,35 @@
 package ru.open.ui;
 
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static ru.open.ui.steps.Steps.*;
+
 public class UITest {
+    @BeforeClass
+    public void StartupMethod() {
+        String browser = "firefox"; // TODO get from config.json
+        Configuration.browser = browser;
+    }
+
+    @Test
+    public void TestMethod() throws InterruptedException {
+        System.out.println("UITest");
+        // TODO get from config.json
+        String startPage = "https://www.google.com";
+
+        // TODO get from testData.json
+        String searchString = "Открытие";
+        String goalPage = "goalPage";
+        String[] currencies = {"USD", "EUR"};
+
+        StartBrowser(); //TODO remove?
+        OpenUrl();
+        WriteInSearchField();
+        ClickSearchButton();
+        CheckSearchResultContainsUrl();
+        OpenTargetResult();
+        CheckCurrenciesSellBuyDelta();
+    }
 }
