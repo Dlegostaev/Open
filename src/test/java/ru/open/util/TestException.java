7b5fd0bca6cb980b86dab5aac3d9e0a82e436ad1
Dlegostaev <smashzoomik@gmail.com>
2020-12-07 23:12:45 +0300

    Added TestException

---
+++ src/test/java/ru/open/util/TestException.java
@@ -0,0 +1,10 @@
+package ru.open.util;
+
+public class TestException extends Exception {
+    public TestException() {
+    }
+
+    public TestException(String message) {
+        super(message);
+    }
+}
